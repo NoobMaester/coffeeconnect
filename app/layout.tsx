41fcc1e6c46ef6c35b00463f93957e049a48e4f9
NoobMaester <joshstilez@gmail.com>
2026-08-29 18:47:33 +0200

    Font update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
+import { Instrument_Sans } from "next/font/google";
 import "./globals.css";
 
-
+const instrumentSans = Instrument_Sans({ subsets: ["latin"], variable: "--font-instrument-sans" });
 
 export const metadata: Metadata = {
   title: "Coffee Connect Center | From Farm to Impact",
-  description: "Coffee Connect Center connects farming, skills, value addition, people and markets to create a sustainable coffee ecosystem that benefits all stakeholders.",
+  description: "CCC connects farming, skills, value addition, people and markets to create a sustainable coffee ecosystem that benefits all stakeholders.",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -13,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html
       lang="en"
     >
-      <body>{children}</body>
+      <body className={instrumentSans.variable}>{children}</body>
     </html>
   );
 }
